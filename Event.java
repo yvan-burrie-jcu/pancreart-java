abdba32b80fc6abaf75cc8e87638168dc705a20b
yvan-burrie <yvan.burrie@hotmail.com>
2020-10-31 10:44:47 +1000

    Update Event.java

--- Event.java
+++ Event.java
@@ -6,18 +6,32 @@ public class Event {
 
     public long userId;
 
+    enum Type {
+        GLUCOSE_READING,
+        INSULIN_INJECTION,
+    }
+
+    public Type type;
+
     public long time;
 
-    public float amount;
+    public double amount;
 
-    public Event(long userId, long time, float amount) {
+    public Event(long userId, Type type, long time, double amount) {
         this.userId = userId;
+        this.type = type;
         this.time = time;
         this.amount = amount;
     }
 
     public Event(JSONObject buffer) throws JSONException {
         userId = buffer.getLong("userId");
+        int typeBuffer = buffer.getInt("type");
+        if (typeBuffer == Type.GLUCOSE_READING.ordinal()) {
+            type = Type.GLUCOSE_READING;
+        } else if (typeBuffer == Type.INSULIN_INJECTION.ordinal()) {
+            type = Type.INSULIN_INJECTION;
+        }
         time = buffer.getLong("time");
         amount = buffer.getFloat("amount");
     }
@@ -25,6 +39,7 @@ public class Event {
     public JSONObject serialize() throws JSONException {
         JSONObject buffer = new JSONObject();
         buffer.put("userId", userId);
+        buffer.put("type", type.ordinal());
         buffer.put("time", time);
         buffer.put("amount", amount);
         return buffer;
